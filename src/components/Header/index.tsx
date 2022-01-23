693014265f661ddf363429a7f69faae7728b4cc0
Fernando <colom_fernando@bitbucket.org>
2022-01-23 00:07:14 -0300

    test api

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import Styles from './styles';
 import InputSearch from 'components/InputSearch';
+import useSWR from 'swr';
+
+const fetcher = (...args: [string]) => fetch(...args).then((res) => res.json());
 
 const Header: React.FC = ({ ...props }) => {
+  const { data } = useSWR(
+    'https://pokeapi.co/api/v2/pokemon/?limit=20',
+    fetcher
+  );
+
+  console.log('data :>> ', data);
   return (
     <Styles.Header {...props}>
       <InputSearch value="pokemon" />
