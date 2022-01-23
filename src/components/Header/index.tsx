a0cdaabfe48d8eeb17d84b7781e5c50ebef68900
Fernando <colom_fernando@bitbucket.org>
2022-01-23 11:21:52 -0300

    dispatch pokemons

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Styles from './styles';
 import InputSearch from 'components/InputSearch';
+import usePokemons from 'hooks/usePokemons';
+import useStore from 'hooks/useStore';
+import { setPokemons } from 'hooks/useStore/actions';
+
+const randomIds = Array.from({ length: 20 }, () =>
+  String(Math.floor(Math.random() * 100))
+);
 
 const Header: React.FC = ({ ...props }) => {
   const [value, setValue] = useState('');
+  const [, dispatch] = useStore();
+  const { data } = usePokemons(randomIds);
 
   const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { target } = event;
@@ -11,6 +20,9 @@ const Header: React.FC = ({ ...props }) => {
     setValue(value);
   };
 
+  useEffect(() => {
+    if (data && data.length) dispatch(setPokemons(data));
+  }, [data]);
   return (
     <Styles.Header {...props}>
       <InputSearch
