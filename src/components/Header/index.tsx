c7ddaea9f692bfecd1864840fe99f200f0d36bf9
Fernando <colom_fernando@bitbucket.org>
2022-01-23 10:01:40 -0300

    add onChange

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Styles from './styles';
 import InputSearch from 'components/InputSearch';
 import useSWR from 'swr';
 
-const fetcher = (...args: [string]) => fetch(...args).then((res) => res.json());
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const Header: React.FC = ({ ...props }) => {
-  const { data } = useSWR(
-    'https://pokeapi.co/api/v2/pokemon/?limit=20',
-    fetcher
-  );
+  const [value, setValue] = useState('');
+  const { data } = useSWR('https://pokeapi.co/api/v2/pokemon/1', fetcher);
+
+  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { target } = event;
+    const { value } = target;
+    setValue(value);
+  };
 
   console.log('data :>> ', data);
   return (
     <Styles.Header {...props}>
-      <InputSearch value="pokemon" />
+      <InputSearch
+        value={value}
+        onChange={handleOnChange}
+        placeholder="Search"
+      />
     </Styles.Header>
   );
 };
--- src/components/InputSearch/index.tsx
+++ src/components/InputSearch/index.tsx
@@ -3,10 +3,18 @@ import Styles from './styles';
 
 interface PropsInputSearch {
   value: string;
+  placeholder?: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const InputSearch: React.FC<PropsInputSearch> = ({ value }) => {
-  return <Styles.Input value={value} />;
+const InputSearch: React.FC<PropsInputSearch> = ({
+  value,
+  onChange,
+  placeholder,
+}) => {
+  return (
+    <Styles.Input value={value} onChange={onChange} placeholder={placeholder} />
+  );
 };
 
 export default InputSearch;
